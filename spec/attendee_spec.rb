38753b0fdfabd6c8eec08de8861c28b9d83d231d
Chris Crane <chrisjcrane@gmail.com>
2023-01-09 08:00:17 -0700

    add tracks_bids item test and #add_bid

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -2,17 +2,27 @@ require './lib/attendee'
 
 describe Attendee do
   before do
-    @attendee = Attendee.new(name: 'Megan', budget: '$50')
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+
+    @attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    @attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    @attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+
+    @auction = Auction.new
   end
 
   describe '#initialize' do
     it 'exists' do
-      expect(@attendee).to be_a(Attendee)
+      expect(@attendee1).to be_a(Attendee)
     end
 
     it 'has attributes' do
-      expect(@attendee.name).to eq("Megan")
-      expect(@attendee.budget).to eq(50)
+      expect(@attendee1.name).to eq("Megan")
+      expect(@attendee1.budget).to eq(50)
     end
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,6 +4,9 @@ describe Item do
   before do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
   end
 
   describe '#initialize' do
@@ -14,6 +17,18 @@ describe Item do
     it 'has attributes' do
       expect(@item1.name).to eq('Chalkware Piggy Bank')
       expect(@item2.name).to eq('Bamboo Picture Frame')
+    end
+
+    it 'tracks bids' do
+      expect(@item1.bids).to eq({})
+    end
+  end
+
+  describe '#add_bid' do
+    it 'adds a new bid to the hash of bids' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
     end
   end
 end
