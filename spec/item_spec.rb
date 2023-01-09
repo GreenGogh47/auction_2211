8de2547f2cdd7589698c5cbc875150e64756ddb8
Chris Crane <chrisjcrane@gmail.com>
2023-01-09 07:30:14 -0700

    Create base files and create item tests.

---
+++ spec/item_spec.rb
@@ -0,0 +1,19 @@
+require './lib/item'
+
+describe Item do
+  before do
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(item1).to be_a(Item)
+    end
+
+    it 'has attributes' do
+      expect(item1.name).to eq('Chalkware Piggy Bank')
+      expect(item2.name).to eq('Bamboo Picture Frame')
+    end
+  end
+end
