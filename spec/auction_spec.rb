2339edc727178205eebcc684a97696abfdf39db9
Chris Crane <chrisjcrane@gmail.com>
2023-01-09 07:46:05 -0700

    ammended tests for clarity

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -12,12 +12,24 @@ describe Auction do
     it 'exists' do
       expect(@auction).to be_a(Auction)
     end
-
+    
     it 'has attributes' do
       expect(@auction.items).to eq([])
-      @auction.add_item(item1)
-      @auction.add_item(item2)
-      expect(@auction.items).to eq([item1, item2])
+    end
+  end
+
+  describe '#add_item' do
+    it "can add items to the array of items" do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      expect(@auction.items).to eq([@item1, @item2])
+    end
+  end
+
+  describe '#item_names' do
+    it 'displays the array of items names' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
       expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
     end
   end
